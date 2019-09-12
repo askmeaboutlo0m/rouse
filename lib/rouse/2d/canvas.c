f552d2bde4a77039c6ce08e0a5e9ad9a519f97b7
askmeaboutloom <askmeaboutloom@live.de>
2019-09-12 22:08:00 +0200

    Pull the nanovg context out of canvas
    It's instead passed to the render function from outside, because the
    context carries a bunch of state such as loaded images that you really
    don't want to manage via the current canvas.

--- lib/rouse/2d/canvas.c
+++ lib/rouse/2d/canvas.c
@@ -21,7 +21,6 @@
 struct R_Canvas {
     R_MAGIC_FIELD
     R_FrameBuffer *fb;
-    NVGcontext    *vg;
     R_Sprite      *sprite;
 };
 
@@ -36,21 +35,10 @@ static R_FrameBuffer *make_fb(int width, int height)
     return R_frame_buffer_new(&options);
 }
 
-static NVGcontext *make_vg(int flags)
-{
-    NVGcontext *vg = nvgCreateGLES2(
-            flags & R_CANVAS_ANTI_ALIAS ? NVG_ANTIALIAS : 0);
-    if (!vg) {
-        R_die("Can't create NanoVG OpenGL ES 2.0 context");
-    }
-    return vg;
-}
-
-R_Canvas *R_canvas_new(int flags, int width, int height)
+R_Canvas *R_canvas_new(int width, int height)
 {
     R_Canvas *canvas = R_NEW_INIT_STRUCT(canvas, R_Canvas,
-            R_MAGIC_INIT(canvas) make_fb(width, height),
-            make_vg(flags), R_sprite_new_root());
+            R_MAGIC_INIT(canvas) make_fb(width, height), R_sprite_new_root());
     R_MAGIC_CHECK(canvas);
     return canvas;
 }
@@ -61,7 +49,6 @@ void R_canvas_free(R_Canvas *canvas)
     if (canvas) {
         R_MAGIC_CHECK_NN(canvas);
         R_sprite_decref(canvas->sprite);
-        nvgDeleteGLES2(canvas->vg);
         R_frame_buffer_free(canvas->fb);
         R_MAGIC_POISON(canvas);
         free(canvas);
@@ -76,17 +63,17 @@ R_Sprite *R_canvas_sprite(R_Canvas *canvas)
 }
 
 
-R_FrameBuffer *R_canvas_render(R_Canvas *canvas)
+R_FrameBuffer *R_canvas_render(R_Canvas *canvas, NVGcontext *vg)
 {
     R_MAGIC_CHECK(canvas);
+    assert(vg && "NVGcontext can't be null");
     R_FrameBuffer *fb = canvas->fb;
-    NVGcontext    *vg = canvas->vg;
 
     R_frame_buffer_bind(fb);
     R_gl_clear(0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0);
 
     nvgBeginFrame(vg, R_int2float(fb->width), R_int2float(fb->height), 1.0f);
-    R_sprite_draw(canvas->sprite, canvas->vg,
+    R_sprite_draw(canvas->sprite, vg,
                   (float[6]){1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f});
     nvgEndFrame(vg);
 

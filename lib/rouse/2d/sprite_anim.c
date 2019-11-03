f1ed56c5913a94cbd92349328f581206ef05e222
askmeaboutloom <askmeaboutloom@live.de>
2019-11-03 13:37:18 -0500

    Add tween functions for all sprite transforms
    Position, shift, scale, skew and angle.

--- lib/rouse/2d/sprite_anim.c
+++ lib/rouse/2d/sprite_anim.c
@@ -51,22 +51,31 @@ static R_AffineTransform *get_transform(void *user)
     return R_sprite_transform_at(data->sprite, data->transform_index);
 }
 
-#define ARGS_TRANSFORM(ARGS) get_transform((ARGS).user.data)
 
+#define DEF_SPRITE_TWEEN(NAME, FIELD) \
+    static float get_ ## NAME(R_TweenCalcArgs args) \
+    { \
+        return get_transform(args.user.data)->FIELD; \
+    } \
+    \
+    static void set_ ## NAME(R_TweenTickArgs args, float value) \
+    { \
+        get_transform(args.user.data)->FIELD = value; \
+    } \
+    \
+    void R_tween_sprite_ ## NAME(R_Step *step, R_Sprite *sprite, \
+                                  int transform_index, R_TweenFloat value) \
+    { \
+        R_tween_add_float(step, value, make_tween_data(sprite, transform_index), \
+                          get_ ## NAME, set_ ## NAME, free_tween_data); \
+    }
 
-static float get_angle(R_TweenCalcArgs args)
-{
-    return ARGS_TRANSFORM(args)->angle;
-}
-
-static void set_angle(R_TweenTickArgs args, float value)
-{
-    ARGS_TRANSFORM(args)->angle = value;
-}
-
-void R_tween_sprite_angle(R_Step *step, R_Sprite *sprite, int transform_index,
-                          R_TweenFloat value)
-{
-    R_tween_add_float(step, value, make_tween_data(sprite, transform_index),
-                      get_angle, set_angle, free_tween_data);
-}
+DEF_SPRITE_TWEEN(pos_x, pos.x)
+DEF_SPRITE_TWEEN(pos_y, pos.y)
+DEF_SPRITE_TWEEN(shift_x, shift.x)
+DEF_SPRITE_TWEEN(shift_y, shift.y)
+DEF_SPRITE_TWEEN(scale_x, scale.x)
+DEF_SPRITE_TWEEN(scale_y, scale.y)
+DEF_SPRITE_TWEEN(skew_x, skew.x)
+DEF_SPRITE_TWEEN(skew_y, skew.y)
+DEF_SPRITE_TWEEN(angle, angle)
--- lib/rouse/2d/sprite_anim.h
+++ lib/rouse/2d/sprite_anim.h
@@ -1,2 +1,26 @@
+void R_tween_sprite_pos_x(R_Step *step, R_Sprite *sprite, int transform_index,
+                          R_TweenFloat value);
+
+void R_tween_sprite_pos_y(R_Step *step, R_Sprite *sprite, int transform_index,
+                          R_TweenFloat value);
+
+void R_tween_sprite_shift_x(R_Step *step, R_Sprite *sprite, int transform_index,
+                            R_TweenFloat value);
+
+void R_tween_sprite_shift_y(R_Step *step, R_Sprite *sprite, int transform_index,
+                            R_TweenFloat value);
+
+void R_tween_sprite_scale_x(R_Step *step, R_Sprite *sprite, int transform_index,
+                            R_TweenFloat value);
+
+void R_tween_sprite_scale_y(R_Step *step, R_Sprite *sprite, int transform_index,
+                            R_TweenFloat value);
+
+void R_tween_sprite_skew_x(R_Step *step, R_Sprite *sprite, int transform_index,
+                           R_TweenFloat value);
+
+void R_tween_sprite_skew_y(R_Step *step, R_Sprite *sprite, int transform_index,
+                           R_TweenFloat value);
+
 void R_tween_sprite_angle(R_Step *step, R_Sprite *sprite, int transform_index,
                           R_TweenFloat value);

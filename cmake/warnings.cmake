767220dbe764e29515a45ec055a647ca0c1f42ef
askmeaboutloom <askmeaboutloom@live.de>
2019-11-26 16:55:06 -0500

    Don't warn about Windows-specific format codes
    MinGW doesn't have %zu, so it has to use non-standard format codes for
    that. We only use those on Windows, so no point in warning about it
    being non-portable.

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -2,4 +2,10 @@ if(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
     set(rouse_warnings -pedantic -pedantic-errors -Wall -Wextra -Wshadow
                        -Wstrict-prototypes -Wmissing-include-dirs -Wconversion
                        -Werror -Wno-error=unused-parameter)
+    # MinGW links against an ancient MSVC runtime that doesn't support %z
+    # modifiers in printf formatting. So we have to use Windows-specific
+    # formats in that case, which we don't want to hear warnings about.
+    if(WIN32)
+        list(APPEND rouse_warnings -Wno-pedantic-ms-format)
+    endif()
 endif()

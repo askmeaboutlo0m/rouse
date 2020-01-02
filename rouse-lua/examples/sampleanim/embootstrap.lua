5b8f52dd46fa0dcec890f848ea6523e08ee5c2bd
askmeaboutloom <askmeaboutloom@live.de>
2020-01-02 01:20:35 +0100

    Fix up sample animation embootstrap
    Give it its proper name and put all actually required assets in it.

---
+++ rouse-lua/examples/sampleanim/embootstrap.lua
@@ -0,0 +1,13 @@
+return {
+    "assets/assetlist",
+    "examples/lib/common.lua",
+    "examples/lib/debuggery.lua",
+    "examples/lib/fpscounter.lua",
+    "examples/lib/preload.lua",
+    "examples/lib/scenebase.lua",
+    "examples/lib/scenesetup.lua",
+    "examples/lib/seqbuilder.lua",
+    "main.lua",
+    "samplescene.lua",
+    "samplesetup.lua",
+}

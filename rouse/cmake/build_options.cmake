4ea134ef5a150baffde98a9e2528f4f14ef1c7db
askmeaboutloom <askmeaboutloom@live.de>
2020-01-05 16:27:28 +0100

    Add build_options.cmake that I somehow forgot

---
+++ rouse/cmake/build_options.cmake
@@ -0,0 +1,4 @@
+option(BUILD_TESTS          "build tests from the 'test' directory"    ON)
+option(BUILD_EXAMPLES       "build examples from 'examples' directory" ON)
+option(BUILD_WITH_GL_CHECKS "enable error checking for OpenGL calls"   ON)
+option(USE_PKGCONFIG        "use pkg-config to find libraries"         ON)

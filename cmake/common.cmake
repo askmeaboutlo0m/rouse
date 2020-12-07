31311f5b4392fd34983bbf1e334f1c6be0d38916
askmeaboutloom <askmeaboutloom@live.de>
2020-12-07 03:42:17 +0100

    Make warnings work for both C and C++

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -7,7 +7,7 @@ if(CMAKE_TOOLCHAIN_FILE)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/build_options.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/warnings.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/asan.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/clang_tidy.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/language_standards.cmake")
---
+++ cmake/flags.cmake
@@ -0,0 +1,15 @@
+if(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
+    set(rouse_common_warnings -pedantic -pedantic-errors -Wall -Wextra -Wshadow
+                              -Wmissing-include-dirs -Wconversion -Werror
+                              -Wno-error=unused-parameter)
+
+    # MinGW links against an ancient MSVC runtime that doesn't support %z
+    # modifiers in printf formatting. So we have to use Windows-specific
+    # formats in that case, which we don't want to hear warnings about.
+    if(CMAKE_C_COMPILER MATCHES "mingw")
+        list(APPEND rouse_common_warnings -Wno-pedantic-ms-format)
+    endif()
+
+    set(rouse_cflags   ${rouse_common_warnings} -Wstrict-prototypes)
+    set(rouse_cxxflags ${rouse_common_warnings} -fno-exceptions)
+endif()

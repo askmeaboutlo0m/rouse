f3253718f8e6a3a5b8303f06c8a2f22baa4566f7
askmeaboutloom <askmeaboutloom@live.de>
2020-12-09 05:07:45 +0100

    Get rid of R.Double in Lua
    It's not really used for anything sensible.

--- rouse-lua/cmake/sources.cmake
+++ rouse-lua/cmake/sources.cmake
@@ -80,7 +80,6 @@ set(rouse_lua_sources
 set(rouse_lua_xls
     "${CMAKE_SOURCE_DIR}/lib/rouse/lua/al.xl"
     "${CMAKE_SOURCE_DIR}/lib/rouse/lua/bitmap.xl"
-    "${CMAKE_SOURCE_DIR}/lib/rouse/lua/double.xl"
     "${CMAKE_SOURCE_DIR}/lib/rouse/lua/ease.xl"
     "${CMAKE_SOURCE_DIR}/lib/rouse/lua/frame_buffer.xl"
     "${CMAKE_SOURCE_DIR}/lib/rouse/lua/frame_renderer.xl"

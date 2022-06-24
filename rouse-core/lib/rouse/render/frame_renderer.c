d8a0e04a7b24ebf2b8b1a5fb0993dc156e509009
askmeaboutloom <askmeaboutloom@live.de>
2022-06-24 09:24:37 +0200

    Get rid of R_Binder usage in R_FrameRenderer
    Instead bind and render stuff manually. This is to eventually get rid of
    R_Binder, since it's not actually useful.

--- rouse-core/lib/rouse/render/frame_renderer.c
+++ rouse-core/lib/rouse/render/frame_renderer.c
@@ -31,15 +31,16 @@
 #include "../common.h"
 #include "../geom.h"
 #include "gl.h"
-#include "binder.h"
 #include "frame_buffer.h"
 #include "frame_renderer.h"
 
 
 struct R_FrameRenderer {
     R_MAGIC_FIELD
-    R_Binder *binder;
-    int      u_ratio, u_sampler;
+    bool         alpha_blending;
+    unsigned int program;
+    unsigned int buffer;
+    int          u_ratio, u_sampler;
 };
 
 
@@ -64,35 +65,20 @@ static const char *frag =
     "    gl_FragColor = texture2D(u_sampler, f_uv);\n"
     "}\n";
 
-
-static void bind(R_UNUSED void *subject, R_UNUSED R_UserData user,
-                 unsigned int buffer, int index)
-{
-    static const float vertices[8] =
-        {-1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f};
-    R_GL(glBindBuffer, GL_ARRAY_BUFFER, buffer);
-    R_GL(glBufferData, GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    R_GL(glVertexAttribPointer, R_int2uint(index), 2, GL_FLOAT, GL_FALSE, 0, NULL);
-}
-
-static void draw(R_UNUSED void *subject, R_UNUSED R_UserData user)
-{
-    R_GL(glDrawArrays, GL_TRIANGLE_STRIP, 0, 4);
-}
+static const float verts[8] =
+    {-1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f};
 
 
 R_FrameRenderer *R_frame_renderer_new(bool alpha_blending)
 {
-    R_Binder *binder = R_binder_new(0, draw, R_user_null(), vert, frag,
-                                    bind, R_user_null(), NULL);
-    if (alpha_blending) {
-        binder->features |= R_BINDER_BLENDING;
-    }
+    unsigned int program = R_gl_program_new(vert, frag);
+    unsigned int buffer;
+    R_GL(glGenBuffers, 1, &buffer);
 
     R_FrameRenderer *fr = R_NEW_INIT_STRUCT(fr, R_FrameRenderer,
-            R_MAGIC_INIT(R_FrameRenderer) binder,
-            R_binder_uniform_location(binder, "u_ratio"),
-            R_binder_uniform_location(binder, "u_sampler"));
+            R_MAGIC_INIT(R_FrameRenderer) alpha_blending, program, buffer,
+            R_gl_uniform_location(program, "u_ratio"),
+            R_gl_uniform_location(program, "u_sampler"));
     R_MAGIC_CHECK(R_FrameRenderer, fr);
     return fr;
 }
@@ -101,7 +87,8 @@ void R_frame_renderer_free(R_FrameRenderer *fr)
 {
     if (fr) {
         R_MAGIC_CHECK(R_FrameRenderer, fr);
-        R_binder_free(fr->binder);
+        R_GL(glDeleteBuffers, 1, &fr->buffer);
+        R_gl_program_free(fr->program);
         R_MAGIC_POISON(R_FrameRenderer, fr);
         free(fr);
     }
@@ -112,8 +99,17 @@ void R_frame_renderer_draw(R_FrameRenderer *fr, R_FrameBuffer *fb)
 {
     R_MAGIC_CHECK(R_FrameRenderer, fr);
     R_MAGIC_CHECK(R_FrameBuffer, fb);
-    R_binder_begin(fr->binder);
+    R_GL(glDisable, GL_DEPTH_TEST);
+    R_GL(glDisable, GL_CULL_FACE);
+    if (fr->alpha_blending) {
+        R_GL(glEnable, GL_BLEND);
+        R_GL(glBlendFunc, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+    else {
+        R_GL(glDisable, GL_BLEND);
+    }
 
+    R_GL(glUseProgram, fr->program);
     R_GL(glActiveTexture, GL_TEXTURE0);
     R_GL(glBindTexture, GL_TEXTURE_2D, fb->color);
     R_GL(glUniform1i, fr->u_sampler, 0);
@@ -121,6 +117,15 @@ void R_frame_renderer_draw(R_FrameRenderer *fr, R_FrameBuffer *fb)
     R_V2 ratio = R_frame_buffer_ratio(fb);
     R_GL(glUniform2f, fr->u_ratio, ratio.x, ratio.y);
 
-    R_binder_draw(fr->binder, NULL);
-    R_binder_end(fr->binder);
+    R_GL(glEnableVertexAttribArray, 0);
+    R_GL(glBindBuffer, GL_ARRAY_BUFFER, fr->buffer);
+    R_GL(glBufferData, GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    R_GL(glVertexAttribPointer, 0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    int max = R_gl_max_vertex_attribs;
+    for (int i = 1; i < max; ++i) {
+        R_GL(glDisableVertexAttribArray, R_int2uint(i));
+    }
+
+    R_GL(glDrawArrays, GL_TRIANGLE_STRIP, 0, 4);
 }

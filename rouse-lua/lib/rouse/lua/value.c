d15c10781dcf28a1fb7dce09aa2b54bd51a4eefc
askmeaboutloom <askmeaboutloom@live.de>
2022-07-27 12:19:36 +0200

    Use main thread in R_LuaValue
    Otherwise this interacts badly with coroutines, which use a separate
    lua_State from the main one.

--- rouse-lua/lib/rouse/lua/value.c
+++ rouse-lua/lib/rouse/lua/value.c
@@ -3,10 +3,18 @@
 #include "intern.h"
 #include "util.h"
 
+static lua_State *get_main_state(lua_State *L)
+{
+    lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_MAINTHREAD);
+    lua_State *main_state = lua_tothread(L, -1);
+    lua_pop(L, 1);
+    return main_state;
+}
+
 R_LuaValue *R_lua_value_new(lua_State *L, int index)
 {
     R_LuaValue *lv = R_NEW_INIT_STRUCT(lv, R_LuaValue,
-            R_MAGIC_INIT(R_LuaValue) L, R_lua_reg_at(L, index));
+            R_MAGIC_INIT(R_LuaValue) get_main_state(L), R_lua_reg_at(L, index));
     R_MAGIC_CHECK(R_LuaValue, lv);
     return lv;
 }
